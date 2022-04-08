52be71a61ca22294bc5551108ab872c4508ec72c
Ray Brennan <ray@quitealright.com>
2022-04-08 17:33:54 +0100

    minor edits

--- Data/RestService.cs
+++ Data/RestService.cs
@@ -6,68 +6,67 @@ using System.Text.Json;
 using System.Threading.Tasks;
 
 
-namespace NoFrixionWpf.Data
+namespace NoFrixionWpf.Data;
+
+
+internal class RestService
 {
 
-    internal class RestService 
+    JsonSerializerOptions serializerOptions;
+
+
+    public RestService()
     {
 
-        JsonSerializerOptions serializerOptions;
-
-
-        public RestService()
+        serializerOptions = new JsonSerializerOptions
         {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            PropertyNameCaseInsensitive = true,
 
-            serializerOptions = new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                PropertyNameCaseInsensitive = true,
-
-                WriteIndented = true
-            };
-        }
-
-
-        internal async Task<Price> GetLatestPriceAsync()
-        {
-
-            Price price = new();
-
-            Uri uri = new Uri(string.Format(Constants.RestUrl, string.Empty));
-
-            try
-            {
-                using HttpClient client = new();
-
-                
-                HttpResponseMessage response = new();
-                
-                //the button is disabled here...
-                response = await client.GetAsync(uri).ConfigureAwait(false);
-
-
-                if (response.IsSuccessStatusCode)
-                {
-                    string content = await response.Content.ReadAsStringAsync();
-
-                    price = JsonSerializer.Deserialize<Price>(content, serializerOptions);
-
-                }
-
-
-
-            }
-            catch (Exception ex)
-            {
-                Debug.WriteLine(@"\tERROR {0}", ex.Message);
-            }
-
-            return price;
-        }
-
-
-
-
+            WriteIndented = true
+        };
     }
+
+
+    internal async Task<Price> GetLatestPriceAsync()
+    {
+
+        Price price = new();
+
+        Uri uri = new Uri(string.Format(Constants.RestUrl, string.Empty));
+
+        try
+        {
+            using HttpClient client = new();
+
+
+            HttpResponseMessage response = new();
+
+            //the button is disabled here...
+            response = await client.GetAsync(uri).ConfigureAwait(false);
+
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+
+                price = JsonSerializer.Deserialize<Price>(content, serializerOptions);
+
+            }
+
+
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(@"\tERROR {0}", ex.Message);
+        }
+
+        return price;
+    }
+
+
+
 }
+
 
